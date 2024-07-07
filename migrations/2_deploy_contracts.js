87f776b1bc79387e1edfc99a0df43d0d6b2176ec
Alex Schechter <alexschechter@Alexs-MacBook-Pro.local>
2024-07-07 15:23:40 +0300

    add contract with the use of token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,13 @@
 var METoken = artifacts.require("METoken");
+var METFaucet = artifacts.require("METFaucet");
 
-module.exports = function(deployer) {
-  // Deploy the METoken contract as our only task
-  deployer.deploy(METoken);
-};
+module.exports = function(deployer, network, accounts) {
+
+	var owner = accounts[0];
+	// Deploy the METoken contract first
+	deployer.deploy(METoken, {from: owner}).then(function() {
+		// Then deploy METFaucet and pass the address of METoken and the
+		// address of the owner of all the MET who will approve METFaucet
+		return deployer.deploy(METFaucet, METoken.address, owner);
+  	});
+}
